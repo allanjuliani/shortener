c1f1d0ba656cfeede036ca4c1d247181646a1207
Allan Juliani <allan.s.juliani@gmail.com>
2022-03-13 00:22:31 -0300

    chore: updated to django 4.0

--- apps/shortener/admin.py
+++ apps/shortener/admin.py
@@ -4,7 +4,7 @@ from django.contrib.admin.models import LogEntry
 from django.contrib.sessions.models import Session
 from django.urls import reverse
 from django.utils.html import format_html
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext_lazy as _
 
 from apps.shortener.models import Log, Shortener
 
