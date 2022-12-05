e9ddaf3892631e57afec94397ff01f734acf388c
Your Name <you@example.com>
2022-12-05 20:30:24 -0300

    chore: removed dark theme app

--- shortener/settings.py
+++ shortener/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.darktheme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
